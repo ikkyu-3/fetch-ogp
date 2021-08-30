8619925510dd4a9c52f0159f854538ce27c38800
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2021-08-30 10:34:47 +0900

    ブラウザ環境考慮部分を削除

--- src/getDomParser.ts
+++ src/getDomParser.ts
@@ -1,10 +1,6 @@
 export const getDomParser = () => {
-  if (typeof DOMParser !== 'undefined') {
-    return DOMParser
-  } else {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const jsdom = require('jsdom')
-    const { JSDOM } = jsdom
-    return new JSDOM().window.DOMParser as typeof DOMParser
-  }
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const jsdom = require('jsdom')
+  const { JSDOM } = jsdom
+  return new JSDOM().window.DOMParser as typeof DOMParser
 }
